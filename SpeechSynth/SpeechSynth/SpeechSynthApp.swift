72abc7da52ac1317677d0cd8254a0ff57c93ea00
Robert Harrison <harrisonrw@gmail.com>
2024-09-05 13:58:55 -0700

    Initial Xcode Project

---
+++ SpeechSynth/SpeechSynth/SpeechSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechSynthApp.swift
+//  SpeechSynth
+//
+//  Created by Robert Harrison on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeechSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
